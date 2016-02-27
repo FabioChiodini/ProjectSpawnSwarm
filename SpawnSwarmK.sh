07b24421b36239405faea28c2c4a61dc6b73ae85
EC2 Default User <ec2-user@ip-172-31-8-186.ec2.internal>
2016-02-27 17:26:56 +0000

    Added Stuff in the script

--- SpawnSwarmK.sh
+++ SpawnSwarmK.sh
@@ -1,3 +1,14 @@
+NC='\033[0m'              #No Color  
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+
 #Load Env variables from File (maybe change to DB)
 #using /home/ec2-user/Cloud1
 #source /home/ec2-user/Cloud1
@@ -46,14 +57,16 @@ docker-machine env SPAWN-$UUIDK > /home/ec2-user/Docker1
 publicipK1=$(docker-machine ip SPAWN-$UUIDK)
 
 #Launches a Container to join the VM to a SWARM Cluster
-docker run -d swarm join --addr=$publicipK1:2376 consul://$publicipCONSULK:8500/swarm
+docker run -d swarm join --addr=$publicipK1:2375 consul://$publicipCONSULK:8500/swarm
 
 
 #Create Docker SWARM VM
 docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION SPAWN-SWARM
 
-#Connects to remote VM
+#Opens Firewall Port for Docker SWARM
+aws ec2 authorize-security-group-ingress --group-name docker-machine --protocol tcp --port 8333 --cidr 0.0.0.0/0
 
+#Connects to remote VM
 docker-machine env SPAWN-SWARM > /home/ec2-user/SWARM1
 . /home/ec2-user/SWARM1
 
@@ -61,7 +74,7 @@ publicipSWARMK=$(docker-machine ip SPAWN-SWARM)
 
 #Prepares SWARM Manager
 
-docker run -d -p 8333:2376 swarm manage consul://$publicipCONSULK:8500/swarm
+docker run -d -p 8333:2375 swarm manage consul://$publicipCONSULK:8500/swarm
 
 
 echo ----
@@ -73,49 +86,15 @@ echo ----
 
 docker -H tcp://$publicipSWARMK:8333 run -d --name www -p 80:80 nginx
 
-echo Connect to $publicipSWARMK Port 80
+echo ${RED}Connect to $publicipSWARMK Port 8333 to manage the Swarm Cluster
+echo Connect to $publicipK1 Port 80 to test the App deployed by Swarm${NC}
+
 
 #KILLS SWARM (Testing purposes)
 docker-machine rm SPAWN-SWARM
 docker-machine rm SPAWN-CONSUL
 docker-machine rm SPAWN-$UUIDK
 
-
-#Spawns VM with UUID
-UUIDK=$(cat /proc/sys/kernel/random/uuid)
-
-#docker-machine create --driver amazonec2 
-docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION SPAWN-$UUIDK
-
-
-echo ---
-echo VM SPAWN-$UUIDK CREATED
-echo ---
-
-#Open port 80 on VM
-aws ec2 authorize-security-group-ingress --group-name docker-machine --protocol tcp --port 80 --cidr 0.0.0.0/0
-
-#Creates a docker container remotely
-#docker-machine env SPAWN-$UUIDK
-#eval $(docker-machine env SPAWN-$UUIDK)
-#eval $(docker-machine env)
-docker-machine env SPAWN-$UUIDK > /home/ec2-user/Docker1
-. /home/ec2-user/Docker1
-
-docker run -d --name docker-nginx -p 80:80 nginx
-
-publicipK=$(docker-machine ip SPAWN-$UUIDK)
-
-echo ----
-echo connect to Public IP
-echo publicipK=$publicipK
-echo ----
-
-
-
-#Gets rid of VM
-#Waits for a Y (to check if all is good)
-docker-machine rm SPAWN-$UUIDK
 
 
 #Displays Public IP
