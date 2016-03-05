9b46563f6b144958f7d1e62cd00254161ff2303f
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-03-05 09:22:27 +0100

    Update SpawnSwarmK.sh

--- SpawnSwarmK.sh
+++ SpawnSwarmK.sh
@@ -46,11 +46,11 @@ echo ----
 
 #Launches another temporary container
 
-docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION localK
+#docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION localK
 
 #Connects to Container
-docker-machine env localK > /home/ec2-user/localK
-. /home/ec2-user/localK
+#docker-machine env localK > /home/ec2-user/localK
+#. /home/ec2-user/localK
 
 #Creates swarm ID and stores it into file and variable
 docker run swarm create > /home/ec2-user/kiodo1
@@ -68,42 +68,11 @@ docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_K
 #Opens Firewall Port for Docker SWARM
 aws ec2 authorize-security-group-ingress --group-name docker-machine --protocol tcp --port 8333 --cidr 0.0.0.0/0
 
-
-
-#Prepares one VM to be joined to SWARM Cluster
-
-#Spawns VM with UUID
-UUIDK=$(cat /proc/sys/kernel/random/uuid)
-
-
-docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION --swarm --swarm-discovery token://$SwarmTokenK SPAWN-$UUIDK 
-
-#I WAS HERE
-
-docker-machine env SPAWN-$UUIDK > /home/ec2-user/Docker1
-. /home/ec2-user/Docker1
-
-publicipK1=$(docker-machine ip SPAWN-$UUIDK)
-
-#Launches a Container to join the VM to a SWARM Cluster
-docker run -d swarm join --addr=$publicipK1:2375 consul://$publicipCONSULK:8500/swarm
-
-
-#Create Docker SWARM VM
-docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION SPAWN-SWARM
-
-#Opens Firewall Port for Docker SWARM
-aws ec2 authorize-security-group-ingress --group-name docker-machine --protocol tcp --port 8333 --cidr 0.0.0.0/0
-
 #Connects to remote VM
 docker-machine env SPAWN-SWARM > /home/ec2-user/SWARM1
 . /home/ec2-user/SWARM1
 
-publicipSWARMK=$(docker-machine ip SPAWN-SWARM)
-
-#Prepares SWARM Manager
-
-docker run -d -p 8333:2375 swarm manage consul://$publicipCONSULK:8500/swarm
+publicipSWARMK=$(docker-machine ip swarm-master)
 
 
 echo ----
@@ -112,19 +81,37 @@ echo publicipSWARMK=$publicipSWARMK
 echo Consul RUNNING ON $publicipCONSULK
 echo ----
 
+
+#Prepares one VM to be joined to SWARM Cluster
+
+#Spawns VM with UUID
+UUIDK1=$(cat /proc/sys/kernel/random/uuid)
+
+
+docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION --swarm --swarm-discovery token://$SwarmTokenK SPAWN-$UUIDK1 
+
+#Stores ip of the VM
+docker-machine env SPAWN-$UUIDK1 > /home/ec2-user/Docker1
+. /home/ec2-user/Docker1
+
+publicipK1=$(docker-machine ip SPAWN-$UUIDK1)
+echo ----
+echo Machine $publicipK1 connected to SWARM
+echo ----
+
+
+
+
 #Spawns VM2 with new UUID
-UUID2K=$(cat /proc/sys/kernel/random/uuid)
+UUIDK2=$(cat /proc/sys/kernel/random/uuid)
 
 #docker-machine create --driver amazonec2
-docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION SPAWN-$UUID2K
+docker-machine create --driver amazonec2 --amazonec2-access-key $K1_AWS_ACCESS_KEY --amazonec2-secret-key $K1_AWS_SECRET_KEY --amazonec2-vpc-id  $K1_AWS_VPC_ID --amazonec2-zone $K1_AWS_ZONE --amazonec2-region $K1_AWS_DEFAULT_REGION --swarm --swarm-discovery token://$SwarmTokenK SPAWN-$UUIDK2
 
-docker-machine env SPAWN-$UUID2K > /home/ec2-user/Docker2
+docker-machine env SPAWN-$UUIDK2 > /home/ec2-user/Docker2
 . /home/ec2-user/Docker2
 
-publicipK2=$(docker-machine ip SPAWN-$UUID2K)
-
-#Launches a Container to join the VM to a SWARM Cluster
-docker run -d swarm join --addr=$publicipK2:2375 consul://$publicipCONSULK:8500/swarm
+publicipK2=$(docker-machine ip SPAWN-$UUIDK2)
 
 echo ----
 echo Second Slave RUNNING ON $publicipK2
@@ -146,7 +133,8 @@ echo run docker -H tcp://$publicipSWARMK:8333 ps TO check swarm status
 #KILLS SWARM (Testing purposes)
 docker-machine rm SPAWN-SWARM
 docker-machine rm SPAWN-CONSUL
-docker-machine rm SPAWN-$UUIDK
+docker-machine rm SPAWN-$UUIDK1
+docker-machine rm SPAWN-$UUIDK2
 
 
 
