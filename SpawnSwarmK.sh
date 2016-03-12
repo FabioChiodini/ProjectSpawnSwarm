2c5449b9e7ce51aa5a2ad9be637f1e632ede5e9a
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-03-12 16:56:58 +0100

    Update SpawnSwarmK.sh

--- SpawnSwarmK.sh
+++ SpawnSwarmK.sh
@@ -107,8 +107,8 @@ if [ $GCEKProvision -eq 1 ]; then
   echo ""
   echo Provisioning VM SPAWN-GCE$j-K
   echo ""
-  gcloud auth login
-  docker-machine create -d google --google-project $K2_GOOGLE_PROJECT --swarm --swarm-discovery token://$SwarmTokenK SPAWN-GCE$j-K
+  
+  docker-machine create -d google --google-project $K2_GOOGLE_PROJECT --google-machine-image ubuntu-1510-wily-v20151114 --swarm --swarm-discovery token://$SwarmTokenK SPAWN-GCE$j-K
   
   #Stores ip of the VM
   docker-machine env SPAWN-GCE$j-K > /home/ec2-user/Docker$j
